9c3151cbe0808d41df54f52057a93c29a2315a5e
Jeroen van Veen <jvanveen@protonmail.com>
2021-01-08 17:51:38 +0100

    chore: remove i18n again

--- src/main.js
+++ src/main.js
@@ -1,23 +1,10 @@
 import './css/pyrite.css'
 import App from './App.vue'
 import { createApp } from 'vue'
-import { createI18n } from 'vue-i18n'
 import FieldText from './components/ui/fields/FieldText.vue'
 import Icon from './components/ui/icons/Icon.vue'
-import nl from './locales/nl.json'
 import Pyrite from './js/app.js'
 import router from './js/router.js'
-
-console.log(createI18n)
-
-
-// const i18n = createI18n({
-//     legacy: false,
-//     locale: 'en',
-//     messages: {
-//         nl,
-//     },
-// })
 
 globalThis.app = new Pyrite(router)
 globalThis.vm = createApp(App)
@@ -27,7 +14,6 @@ globalThis.vm.component('FieldText', FieldText)
 
 globalThis.vm
     .use(router)
-    // .use(i18n)
     .mount('#app')
 
 
