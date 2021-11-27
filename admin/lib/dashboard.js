5bfeb12fe5aa89901fafdf9c7ea609495b61e9b6
Jeroen van Veen <jvanveen@protonmail.com>
2021-11-27 18:38:29 +0100

    fix: invalid setting

--- admin/lib/dashboard.js
+++ admin/lib/dashboard.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch'
 
 export async function loadStats(groupId) {
     const headers = new fetch.Headers()
-    const {username, password} = app.settings.sfu.admin
+    const {username, password} = app.config.sfu.admin
     const authHeader = `Basic ${Buffer.from(`${username}:${password}`, 'utf-8').toString('base64')}`
     headers.append('Authorization', authHeader)
     const stats = await (await fetch(`${app.settings.sfu.url}/stats.json`, {headers})).json()
